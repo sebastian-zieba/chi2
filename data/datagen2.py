551b4b40d2da00ca1f6685abc91cf2e87defda7c
Sebastian (X1) <zieba@mpia-hd.mpg.de>
2021-06-03 00:26:42 +0200

    added datagen2.py

---
+++ data/datagen2.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#y = kx + d
+
+k = 1.234
+d = -43.21
+
+x = np.linspace(-100,100,100)
+y = k * x + d + np.random.randn(len(x))
+y_err = [1]*len(x)
+plt.scatter(x,y, s=1)
+plt.plot(x, k*x+d, c='r')
+plt.plot(x, k*x+d+1, c='r', ls='--')
+plt.plot(x, k*x+d-1, c='r', ls='--')
+plt.show()
+
+np.savetxt('datalist.txt', list(zip(x,y,y_err)))
